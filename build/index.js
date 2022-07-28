a2211d8d772a78cea8319a68a9a6829cb1a460fe
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-07-28 07:26:54 -0700

    sorting implimentation

--- build/index.js
+++ build/index.js
@@ -1,2 +1,21 @@
 "use strict";
-console.log('hi');
+class Sorter {
+    constructor(collection) {
+        this.collection = collection;
+    }
+    sort() {
+        const { length } = this.collection;
+        for (let i = 0; i < length; i++) {
+            for (let j = 0; j < length - i - 1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    const left = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = left;
+                }
+            }
+        }
+    }
+}
+const sorter = new Sorter([10, 2, -2, 4]);
+sorter.sort();
+console.log(sorter.collection);
