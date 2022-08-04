acf1c1711385c2dc12ba5ba718055e2f48878d5c
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-08-04 11:24:18 -0700

    test and linkedlist sorter working

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,23 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 
-const charactersCollection = new CharactersCollection('hello');
-const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
-const sorter2 = new Sorter(charactersCollection);
+// const charactersCollection = new CharactersCollection('hello');
+// const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+// const sorter = new Sorter(numbersCollection);
+// const sorter2 = new Sorter(charactersCollection);
+// sorter.sort();
+// sorter2.sort();
+// console.log(numbersCollection.data);
+// console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-sorter2.sort();
-console.log(numbersCollection.data);
-console.log(charactersCollection.data);
+linkedList.print();
