a19941be62c178790ea89cd82abd548807adb525
Matt-Chisholm <mathesonchisholm@hotmail.com>
2022-08-04 10:24:21 -0700

    testing

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { Sorter } from "./Sorter";
 import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
+const charactersCollection = new CharactersCollection('hello');
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 const sorter = new Sorter(numbersCollection);
+const sorter2 = new Sorter(charactersCollection);
 sorter.sort();
+sorter2.sort();
 console.log(numbersCollection.data);
+console.log(charactersCollection.data);
